f7af67f4ea2bad2fccc89e1abbbc81dcfc3589a0
Bridget Cahill <you@example.com>
2015-12-07 11:55:09 -0600

    broken solution

--- calculator.php
+++ calculator.php
@@ -43,7 +43,7 @@ class Calculator {
       $this->multiplication($array);
 
     }elseif($array[1] == "/") {
-  $this->division($array);
+    $this->division($array);
 
     }elseif($array[1] == "^") {
       $this->exponents($array);
@@ -57,7 +57,7 @@ class Calculator {
     $first_num = reset($array);
     $second_num = end($array);
 
-    return $this->result = floatval($first_num) + floatval($second_num);
+    $this->result = floatval($first_num) + floatval($second_num);
 
   }
 
@@ -65,7 +65,7 @@ class Calculator {
     $first_num = reset($array);
     $second_num = end($array);
 
-    return $this->result = floatval($first_num) - floatval($second_num );
+    $this->result = floatval($first_num) - floatval($second_num );
 
   }
 
@@ -73,7 +73,7 @@ class Calculator {
     $first_num = reset($array);
     $second_num = end($array);
 
-    return $this->result = floatval($first_num) * floatval($second_num );
+    $this->result = floatval($first_num) * floatval($second_num );
 
   }
 
@@ -81,7 +81,7 @@ class Calculator {
     $first_num = reset($array);
     $second_num = end($array);
 
-    return $this->result = floatval($first_num) / floatval($second_num );
+    $this->result = floatval($first_num) / floatval($second_num );
 
   }
 
@@ -89,7 +89,7 @@ class Calculator {
     $first_num = reset($array);
     $second_num = end($array);
 
-    return $this->result = pow(floatval($first_num), floatval($second_num));
+    $this->result = pow(floatval($first_num), floatval($second_num));
 
   }
 
@@ -103,26 +103,132 @@ class Calculator {
     return $place_holder;
   }
 
+  // public function calculateEquation($array){
+  //   $length = count($array);
+  //   for ($i = 0; $i <= $length; $i++ ){
+  //     if ($i < $length){
+  //       $this->selectOperator($array[$i]);
+  //       $array[$i + 1][0] = $this->result;
+  //       unset($array[$i]);
 
-//takes in array which holds multiple sub arrays
-//outputs result, thats a completely calculated equation
+  //     } else {
+  //       $this->selectOperator($array[$i]);
+  //     }
+  //   }
+  //   return $this->result;
+  // }
 
-  public function calculateEquation($array){
+  //input array that hasnt taken into account the order of operations
 
-    $length = count($array);
-    for ($i = 0; $i <= $length; $i++ ){
-      if ($i < $length){
-        $this->selectOperator($array[$i]);
-        $array[$i + 1][0] = $this->result;
-        unset($array[$i]);
 
-      } else {
-        $this->selectOperator($array[$i]);
+  // public function orderOperations($array){
+  //   //first check, the array
+  //   findExponent($array);
+
+  // }
+
+  public function findExponent($array){
+    $updated_array = array();
+    for($i = 0; $i <= count($array); $i++ ){
+      if ($array[$i][1] == "^"){
+          $this->selectOperator($array[$i]);
+          $array[$i] = array($this->result);
+
+          var_dump($array);
+          $updated_array = $array;
+          var_dump($updated_array);
+          $updated_array = $this->replaceNeighbors($updated_array, $i, $this->result);
+          // var_dump($updated_array);
+          unset($updated_array[$i]);
+          // var_dump($updated_array);
+          $this->findExponent($updated_array);
+        } else {
+          $this->findMultiDivi($updated_array);
+        }
       }
     }
-    return $this->result;
+
+  public function findMultiDivi($array){
+    $updated_array = array();
+    for($i = 0; $i <= count($array); $i++ ){
+        if ($array[$i][1] == "*" || $array[$i][1] == "/"){
+          $this->selectOperator($array[$i]);
+          $array[$i] = array($this->result);
+          $updated_array = $array;
+
+          $updated_array = $this->replaceNeighbors($updated_array, $i, $this->result);
+
+          unset($updated_array[$i]);
+          $this->findMultiDivi($updated_array);
+        } else {
+          $this->findAddSub($updated_array);
+        }
+      }
+    }
+
+    public function findAddSub($array){
+      for($i = 0; $i <= count($array); $i++ ){
+      if ($array[$i][1] == "+" || $array[$i][1] == "-"){
+        $this->selectOperator($array[$i]);
+        $array[$i] = array($this->result);
+        $updated_array = $array;
+
+        $updated_array = $this->replaceNeighbors($updated_array, $i, $this->result);
+
+        unset($updated_array[$i]);
+        $this->findAddSub($updated_array);
+      } else {
+        return $this->result;
+        }
+      }
+    }
+
+  public function hasNeighbors($array, $index){
+    if (($array[$index + 1] == true) || ($array[$index - 1] ==true)) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
+  public function replaceNeighbors($array, $index, $value){
+
+    if ($this->hasNeighbors($array, $index) == true){
+      if($array[$index + 1] == true && $array[$index - 1] == true){
+
+        $array[$index + 1][0] = $value;
+        $array[$index -1][-1] = $value;
+
+      }elseif($array[$index + 1] == true){
+
+        $array[$index + 1][0] = $value;
+
+      }elseif($array[$index - 1] == true){
+        $array[$index -1][-1] = $value;
+
+      } else {
+        //should never actually get here
+        return $array;
+      }
+    } else {
+      return $array;
+    }
+  }
+
+
+
 }
+
+
+// 1 + 4 / 2 = 3
+//input of reorder function
+// [1 + 4], [4 / 2]
+//output of reorder function
+//[4 / 2], [1 + 4]
+//evaluate
+//[2], [1 + 4]
+//replace
+//[1 + 2]
+
 
 ?>
--- runner.php
+++ runner.php
@@ -10,29 +10,10 @@ $calculator->splitString();
 
 $calculator->num_array = $calculator->breakDownArray($calculator->num_array, 3);
 
-$calculator->calculateEquation($calculator->num_array);
+$calculator->findExponent($calculator->num_array);
 
-//check first
-//if one contains ^ run the exponent run function and replace the last value of the previous array and replace the first index of the next array
-//then delete that array that holds one value
+// $calculator->calculateEquation($calculator->num_array);
 
-//check second
-//continue this for / and *
-//still delete the evaluated arrays that would only hold one number
-
-//check last
-//lastly do + or -
-//flatten end result and return as a number
-
-// then if one contains / or *
-// run function and store that value in that array
-
-//then if one contains + or -
-//run function
-
-
-//handle how many operators are in the string
-//currently only handling one operator
 
 
 $calculator->roundNumber($calculator->result);
